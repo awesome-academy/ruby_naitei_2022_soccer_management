f2caaf8429dd1f7727cc110a0d98d5bb37ffaa8f
VuMinhCong <minhcong.vu99@gmail.com>
2022-08-23 23:57:39 +0700

    Sign up_after_rebase#2

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "3.0.2"
 
+gem "bcrypt", "~> 3.1.7"
 gem "bootsnap", ">= 1.4.4", require: false
 gem "bootstrap-sass", "3.4.1"
 gem "config"
@@ -26,6 +27,7 @@ end
 
 group :development, :test do
   gem "pry", "~> 0.14.0"
+  gem "pry-rails"
   gem "rubocop", "~> 1.26", require: false
   gem "rubocop-checkstyle_formatter", require: false
   gem "rubocop-rails", "~> 2.14.0", require: false
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module RubyNaitei2022SoccerManagement
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
