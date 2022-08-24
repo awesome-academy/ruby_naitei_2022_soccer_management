8be18c6362ae936b7e964be29a8044da59735e7f
Minh Mai <maihoangminhbk@gmail.com>
2022-08-24 09:42:41 +0700

    Add search socer fields

--- app/controllers/soccer_fields_controller.rb
+++ app/controllers/soccer_fields_controller.rb
@@ -1,6 +1,8 @@
 class SoccerFieldsController < ApplicationController
   def index
-    @pagy, @soccer_fields = pagy(SoccerField.all,
+    @pagy, @soccer_fields = pagy(SoccerField
+                                  .search_name(params[:name])
+                                  .search_address(params[:address]),
                                  items: Settings.pagy.soccer_fields)
   end
 end
