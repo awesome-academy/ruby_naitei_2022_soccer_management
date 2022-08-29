ccd4fb2f2ee086632e24381a3ebd7c2e358f2661
VuMinhCong <minhcong.vu99@gmail.com>
2022-08-29 17:19:33 +0700

    Decentralization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i(edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: %i(destroy)
+
   def new
     @user = User.new
   end
@@ -25,6 +29,25 @@ class UsersController < ApplicationController
 
   def edit; end
 
+  def update
+    if @user.update user_params
+      flash[:success] = t ".update_success"
+      redirect_to @user
+    else
+      flash[:danger] = t ".update_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".user_deleted"
+    else
+      flash[:danger] = t ".delete_fail"
+    end
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(
@@ -33,5 +56,24 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".danger"
+    redirect_to login_path
+  end
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:error] = t ".incorrect_user"
+    redirect_to root_path
+  end
+
+  def admin_user
+    redirect_to root_path unless user.role == 1
   end
 end
