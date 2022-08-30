e273ea2eda2c249a8ae6c81e7fcb106716d04395
Minh Mai <maihoangminhbk@gmail.com>
2022-08-30 16:42:04 +0700

    Show soccer field

--- app/controllers/soccer_fields_controller.rb
+++ app/controllers/soccer_fields_controller.rb
@@ -3,4 +3,12 @@ class SoccerFieldsController < ApplicationController
     @pagy, @soccer_fields = pagy(SoccerField.all,
                                  items: Settings.pagy.soccer_fields)
   end
+
+  def show
+    @soccer_field = SoccerField.find_by id: params[:id]
+    return if @soccer_field
+
+    flash[:error] = t ".not_found"
+    redirect_to root_path
+  end
 end
