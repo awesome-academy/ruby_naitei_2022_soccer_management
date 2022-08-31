b1d1e3dd7297408428335e38152832b1a1fe4797
tranxuantruongjk <xuantruonghy823@gmail.com>
2022-08-31 10:06:36 +0700

    field detail

--- app/controllers/admin/soccer_fields_controller.rb
+++ app/controllers/admin/soccer_fields_controller.rb
@@ -1,18 +1,20 @@
 class Admin::SoccerFieldsController < Admin::BaseController
+  before_action :find_field, except: %i(index new create)
+
   def new
     @soccer_field = SoccerField.new
   end
 
-  def show
-    @soccer_field = SoccerField.find_by id: params[:id]
-    return if @soccer_field
-
-    flash[:danger] = t(".not_found")
-    redirect_to admin_root_path
+  def index
+    @pagy, @soccer_fields = pagy(SoccerField.all,
+                                 items: Settings.pagy.soccer_fields)
   end
+
+  def show; end
 
   def create
     @soccer_field = SoccerField.new(soccer_field_params)
+    @soccer_field.image.attach(params[:soccer_field][:image])
     if @soccer_field.save
       flash[:success] = t ".success"
       redirect_to admin_soccer_field_path(@soccer_field)
@@ -22,9 +24,38 @@ class Admin::SoccerFieldsController < Admin::BaseController
     end
   end
 
+  def edit; end
+
+  def update
+    if @soccer_field.update(soccer_field_params)
+      flash[:success] = t ".success"
+      redirect_to admin_soccer_field_path(@soccer_field)
+    else
+      flash[:danger] = t ".failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @soccer_field.destroy
+      flash[:success] = t(".success")
+    else
+      flash[:danger] = t(".failed")
+    end
+    redirect_to admin_soccer_fields_path
+  end
+
   private
   def soccer_field_params
     params.require(:soccer_field).permit(:name, :address, :field_type,
-                                         :rate, :hour_price)
+                                         :rate, :hour_price, :image)
+  end
+
+  def find_field
+    @soccer_field = SoccerField.find_by id: params[:id]
+    return if @soccer_field
+
+    flash[:danger] = t(".not_found")
+    redirect_to admin_root_path
   end
 end
--- app/models/soccer_field.rb
+++ app/models/soccer_field.rb
@@ -1,4 +1,5 @@
 class SoccerField < ApplicationRecord
+  has_one_attached :image
   validates :name, presence: true,
                    length: {maximum: Settings.soccer_field.name_max_length}
 
@@ -7,4 +8,17 @@ class SoccerField < ApplicationRecord
 
   validates :field_type, :hour_price, presence: true,
                                       numericality: {only_integer: true}
+
+  validates :image, content_type: {in: Settings.image.type,
+                                   message: :valid_format},
+                    size: {less_than: Settings.image.size.megabytes,
+                           message: :should_less_than}
+
+  def display_image
+    image.variant resize_to_limit: Settings.image.resize_to_limit
+  end
+
+  def display_preview_image
+    image.variant resize_to_limit: Settings.image.resize_to_limit_preview
+  end
 end
